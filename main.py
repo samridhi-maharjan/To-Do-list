4d4078d55fbb3432f72a9f7f610313001742ab5e
samridhi-maharjan <samridhi.maharjan111@gmail.com>
2025-07-17 02:15:29 +0545

    update addTask

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-tasks=[]
-# if _name_ == '_main_':
+import numpy as np
 
-# main function
+lists=[]
 def main():
     print("TO-DO LIST")
     menu()
@@ -12,13 +11,13 @@ def menu():
         try:
             print("Welcome to the To-Do List app")
             print("Please select on of the operations")
-            print("1. Add task")
+            print("1. Add Task")
             print("2. View Task List")
             print("3. Mark Task Complete")
             print("4. Remove task")
             print("5. Exit app")
             
-            choice=int(input())     # get input from user
+            choice=int(input("Press number of the desired operation"))     # get input from user
             # compare user's input with the operation index
             if choice==1:
                 addTask()
@@ -32,12 +31,14 @@ def menu():
                 closeApp()
                 break
             else:
-                print("error")
+                print("Please enter number from 1 to 5")       #catches integer error
         except ValueError:
-            print("invalid input")
+            print("invalid input")     #catches string error
 
 def addTask():
-    print("add")
+    print("Enter your task")
+    taskName=input("please enter one task at a time")
+    print(taskName)
 def viewTask():
     print("view")
 def completeTask():
